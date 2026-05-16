e72d83035bce4887f65bd8af62c51abb02925f37
connormorrisonza <connor.morrison.za@gmail.com>
2026-05-16 15:09:47 +0200

    Added bash script to loop simulate many different configurations automatically and output them and the evaluation metrics into a .csv file

--- simulation.sh
+++ simulation.sh
@@ -1,15 +1,15 @@
 echo "total_patrons,schedalg,seed,patron,drink,priority,queue_level,seq_no,service_start_time,arrival_time,completion_time,waiting_time,imbibing_time,turnaround_time,enqueue_time,execution_time,response_time" > simulation_results.csv
-for patrons in 5
+for patrons in 5 10 20 50
 do
-    for seed in 1 #{1..2}
+    for seed in {1..50}
     do
-        for schedalg in 0
+        for schedalg in 0 1 2 3
         do
 			#args format ARGS="patrons scheduling_algorithm 0 seed"
 			#(echo -n "$patrons,$schedalg,$seed," >&2; make --silent run ARGS="$patrons $schedalg 0 $seed") 2>> simulation_results.csv
 			make --silent run ARGS="$patrons $schedalg 0 $seed" 2>&1 >/dev/null | sed "s/^/$patrons,$schedalg,$seed,/" >> simulation_results.csv
         done
     done
-done #2>> simulation_results.csv
+done
 # Outputs the results into simulation_results.csv in the format:
 # patron,drink,priority,queue_level,seq_no,service_start_time,arrival_time,completion_time,waiting_time,imbibing_time,turnaround_time,enqueue_time,execution_time,response_time
