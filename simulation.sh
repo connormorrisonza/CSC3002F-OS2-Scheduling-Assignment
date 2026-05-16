cc1f4f18571d73e631f5905717f7df1252a7a284
connormorrisonza <connor.morrison.za@gmail.com>
2026-05-16 23:04:05 +0200

    Finshed the complete simulation

--- simulation.sh
+++ simulation.sh
@@ -1,12 +1,11 @@
 echo "total_patrons,schedalg,seed,patron,drink,priority,queue_level,seq_no,service_start_time,arrival_time,completion_time,waiting_time,imbibing_time,turnaround_time,enqueue_time,execution_time,response_time" > simulation_results.csv
-for patrons in 5 10 20 50
+for patrons in 5 20 50
 do
-    for seed in {1..50}
+    for seed in {1..30}
     do
         for schedalg in 0 1 2 3
         do
 			#args format ARGS="patrons scheduling_algorithm 0 seed"
-			#(echo -n "$patrons,$schedalg,$seed," >&2; make --silent run ARGS="$patrons $schedalg 0 $seed") 2>> simulation_results.csv
 			make --silent run ARGS="$patrons $schedalg 0 $seed" 2>&1 >/dev/null | sed "s/^/$patrons,$schedalg,$seed,/" >> simulation_results.csv
         done
     done
